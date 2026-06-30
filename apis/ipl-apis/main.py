948cf4fbab5ae10a0baaa002747ab15933200161
krunal vekariya <krunalvekariya12345@gmail.com>
2026-06-30 09:03:35 +0530

    added db initilization into the fastapi lifespan from the depecrated startup event

--- apis/ipl-apis/main.py
+++ apis/ipl-apis/main.py
@@ -1,19 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from routers import users, teams, auth
 
 from utils.user_utils import create_db_and_tables
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(users.router)
 app.include_router(teams.router)
 app.include_router(auth.router)
-
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
 
 
 @app.get("/")
